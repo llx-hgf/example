05d93d8ba991514306f63ad099692ee5e9112fdb
llx-hgf <=>
2020-04-28 11:26:02 +0800

    init repo

---
+++ src/main/java/com/example/huanggf/HuanggfApplication.java
@@ -0,0 +1,13 @@
+package com.example.huanggf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuanggfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuanggfApplication.class, args);
+    }
+
+}
